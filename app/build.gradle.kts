cef2919d1e4e9fd3d634e82362dcfcfc512b9755
Aurian Lira <aurianlira972@gmail.com>
2025-07-03 17:57:10 -0300

    Prática 6 - Parte 1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,6 +55,7 @@ dependencies {
     implementation(libs.androidx.benchmark.macro)
     implementation(libs.androidx.ui.text.android)
     implementation(libs.firebase.auth)
+    implementation(libs.firebase.firestore)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
