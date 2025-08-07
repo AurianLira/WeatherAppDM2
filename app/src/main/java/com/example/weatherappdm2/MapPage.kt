21b24520ea20801b19e617869c7cd0993fd04d74
Aurian Lira <aurianlira972@gmail.com>
2025-08-07 15:45:49 -0300

    Prática 9 - Parte 2

--- app/src/main/java/com/example/weatherappdm2/MapPage.kt
+++ app/src/main/java/com/example/weatherappdm2/MapPage.kt
@@ -2,6 +2,7 @@ package com.example.weatherappdm2
 
 import android.Manifest
 import android.content.pm.PackageManager
+import android.graphics.Bitmap
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -11,22 +12,25 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.core.content.ContextCompat
+import androidx.core.graphics.drawable.toBitmap
 import com.example.weatherappdm2.viewmodel.MainViewModel
 import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
-import com.google.maps.android.compose.*
+import com.google.maps.android.compose.GoogleMap
+import com.google.maps.android.compose.MapProperties
+import com.google.maps.android.compose.MapUiSettings
+import com.google.maps.android.compose.Marker
+import com.google.maps.android.compose.MarkerState
+import com.google.maps.android.compose.rememberCameraPositionState
 
 @Composable
 fun MapPage(
     modifier: Modifier = Modifier,
     viewModel: MainViewModel
 ) {
-    val recife = LatLng(-8.05, -34.9)
-    val caruaru = LatLng(-8.27, -35.98)
-    val joaopessoa = LatLng(-7.12, -34.84)
     val camPosState = rememberCameraPositionState()
-
     val context = LocalContext.current
+
     val hasLocationPermission by remember {
         mutableStateOf(
             ContextCompat.checkSelfPermission(
@@ -52,30 +56,26 @@ fun MapPage(
                         viewModel.loadWeather(it.name)
                     }
                 }
+                LaunchedEffect(it.weather) {
+                    if (it.weather != null && it.weather!!.bitmap == null) {
+                        viewModel.loadBitmap(it.name)
+                    }
+                }
+                val image = it.weather?.bitmap ?: ContextCompat.getDrawable(context, R.drawable.loading)!!
+                    .toBitmap()
+                val marker = BitmapDescriptorFactory
+                    .fromBitmap(image.scale(120, 120))
                 Marker(
-                    state = MarkerState(position = it.location!!),
+                    state = MarkerState(position = it.location),
                     title = it.name,
+                    icon = marker,
                     snippet = it.weather?.desc ?: "Carregando..."
                 )
             }
         }
-        Marker(
-            state = MarkerState(position = recife),
-            title = "Recife",
-            snippet = "Marcador em Recife",
-            icon = BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE)
-        )
-        Marker(
-            state = MarkerState(position = caruaru),
-            title = "Caruaru",
-            snippet = "Marcador em Caruaru",
-            icon = BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_YELLOW)
-        )
-        Marker(
-            state = MarkerState(position = joaopessoa),
-            title = "João Pessoa",
-            snippet = "Marcador em João Pessoa",
-            icon = BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED)
-        )
     }
+}
+
+fun Bitmap.scale(width: Int, height: Int): Bitmap {
+    return Bitmap.createScaledBitmap(this, width, height, false)
 }
